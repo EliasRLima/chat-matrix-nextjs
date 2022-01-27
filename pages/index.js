eea76c932c85ec4563bba5c0c860350ec90c7c2b
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-01-27 10:51:22 -0300

    chat_config
    Preparando o ambiente para o nextjs.

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function HomePage() {
+    return (
+        <div>
+           <h1>Bem vindo(a)!</h1>
+           <h2>Discord - Matrix</h2>
+
+           <style jsx>{
+            `h1 {
+              color: red;
+            }` 
+           }
+              
+           </style>
+        </div>)
+  }
+  
+  export default HomePage
